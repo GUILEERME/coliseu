2538de2b51f8fe37a91e4949fc2284c53479d9f5
GUILEERME <guilhermealtomare@gmail.com>
2025-06-24 14:30:47 -0300

    Criar CSS do menu

--- includes/menu.php
+++ includes/menu.php
@@ -1,1 +1,18 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <div class="menu">
+        <ul class="menu">
+            <a href=""> Login </a>
+            <li> <a href="torneios.php"> Torneios </a>  </li>
+            <li> <a href="criarTorneio.php">Criar</a>  </li>
+        </ul>
+    </div>
+</body>
+</html>
