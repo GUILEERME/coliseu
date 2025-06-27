67d34f425b395e9a24c963fac637c2fec2103fcd
GUILEERME <guilhermealtomare@gmail.com>
2025-06-27 11:08:52 -0300

    Criar formulário de cadastro

---
+++ pages/cadastro.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <?php
+    include('../includes/menu.php');
+    ?>
+
+    <div class="container-cadastro">
+        <form class="form-cadastro" action="">
+            <h1> Cadastrar-se</h1>
+            <h2>Descubra e participe de eventos de jogos</h2>
+            <input type="email" name="Email" id="" placeholder="Email">
+            <input type="password" name="" id="" placeholder="Senha">
+            <a href="login.php"> Já tenho conta </a>
+            <button type="submit"> Cadastrar-se </button>
+        </form>
+    </div>
+
+</body>
+</html>
